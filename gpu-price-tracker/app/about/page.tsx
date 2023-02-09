81e2c77c81881591ddf09a6fc32c65a9daab4c4b
Zivic <djzivic@gmail.com>
2023-02-09 22:26:31 +0100

    Initial setup NextJS, TypeScript and TailwindCSS

---
+++ gpu-price-tracker/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
+  }
+  
---
+++ gpu-price-tracker/app/main.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+function Main() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About Us</Link>
+      </li>
+      <li>
+        <Link href="/blog/hello-world">Blog Post</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Main
