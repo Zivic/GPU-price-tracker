cb2db1a2a13b134d230cb807553289604e8f97b5
Zivic <djzivic@gmail.com>
2023-02-10 21:22:55 +0100

    add gpu and donut models

--- gpu-price-tracker/components/ThreeCanvas.tsx
+++ gpu-price-tracker/components/ThreeCanvas.tsx
@@ -1,6 +1,9 @@
-"use client"
+"use client";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import React, { useEffect } from "react";
+
 const ThreeCanvas = () => {
   useEffect(() => {
     //Initialization of ThreeJS canvas
@@ -19,11 +22,50 @@ const ThreeCanvas = () => {
     camera.position.setZ(30);
 
     renderer.render(scene, camera);
+
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const torus = new THREE.Mesh(geometry, material);
+
+    scene.add(torus);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(25, 5, 5);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(pointLight, ambientLight);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    const loader = new FBXLoader();
+    loader.load(
+      "/models/GPU/source/model.fbx",
+      (object) => {
+        scene.add(object),
+          (xhr: ProgressEvent<EventTarget>) => {
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+          },
+          () => {
+            console.error("error");
+          };
+      }
+    );
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.005;
+      torus.rotation.z += 0.01;
+
+      controls.update();
+
+      renderer.render(scene, camera);
+    }
+    animate();
   }, []);
 
-  return(
-    <canvas id="bg">
-    </canvas>)
+  return <canvas id="bg"></canvas>;
 };
 
 export default ThreeCanvas;
