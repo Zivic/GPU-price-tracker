81213c8226d85f8f3d21de9202a79ee6cb5aeab8
Zivic <djzivic@gmail.com>
2023-02-11 19:48:52 +0100

    refactor: clean up canvas code and models Remove unnecessary code and old .fbx model

--- gpu-price-tracker/components/ThreeCanvas.tsx
+++ gpu-price-tracker/components/ThreeCanvas.tsx
@@ -1,10 +1,8 @@
 "use client";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import React, { useEffect } from "react";
-import { Vector3 } from "three";
 
 const ThreeCanvas = () => {
   useEffect(() => {
@@ -21,15 +19,16 @@ const ThreeCanvas = () => {
       antialias: true,
       alpha: true,
     });
+
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.autoClear = false;
     renderer.setClearColor(0x000000, 0.0);
     // renderer.physicallyCorrectLights = true;
-    camera.position.setZ(30);
-
+    camera.position.setZ(10);
     renderer.render(scene, camera);
 
+    //Creating Torus, TODO: remove after testing
     const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
     const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
     const torus = new THREE.Mesh(geometry, material);
@@ -37,33 +36,21 @@ const ThreeCanvas = () => {
 
     scene.add(torus);
 
+    //Creating light sources
     const pointLight = new THREE.PointLight(0xffffff, 3);
     pointLight.position.set(25, 15, 5);
     const pointLight2 = new THREE.PointLight(0xffffff, 3);
     pointLight.position.set(0, 10, 10);
-
     const ambientLight = new THREE.AmbientLight(0x404040);
     scene.add(pointLight,pointLight2, ambientLight);
 
     const controls = new OrbitControls(camera, renderer.domElement);
 
-    // const loader = new FBXLoader();
-    // loader.load("/models/GPU/source/model.fbx", (object) => {
-    //   scene.add(object),
-    //     (xhr: ProgressEvent<EventTarget>) => {
-    //       console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    //     },
-    //     () => {
-    //       console.error("error");
-    //     };
-    // });
-    var model: any
-
+    //Importing GPU model
     const gltfLoader = new GLTFLoader();
     gltfLoader.load(
       "/models/RTX3090/RTX3090.glb",
       (gltf) => {
-        model = gltf;
         scene.add(gltf.scene);
         gltf.animations; // Array<THREE.AnimationClip>
         gltf.scene; // THREE.Group
@@ -87,20 +74,16 @@ const ThreeCanvas = () => {
       torus.rotation.y += 0.005;
       torus.rotation.z += 0.002;
 
-      // if(model && model.rotation){
-      //   model.rotation.x += 0.005;
-      //   model.rotation.y += 0.005;
-      //   model.rotation.z += 0.002;
-      // }
+      //GPU rotation, array index needs to be adjusted if more assets are added
       if(scene.children[4]){
-      scene.children[4].rotation.x += 0.005;
-      scene.children[4].rotation.y += 0.005;
+      scene.children[4].rotation.x += 0.0015;
+      scene.children[4].rotation.y += 0.0015;
       scene.children[4].rotation.z += 0.0005;
       }
       controls.update();
-
       renderer.render(scene, camera);
     }
+
     animate();
   }, []);
 
