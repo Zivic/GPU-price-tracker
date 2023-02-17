fde55a3a0ca31eceda08c286f11126850b079d55
Zivic <djzivic@gmail.com>
2023-02-17 18:48:36 +0100

    feat: add successful scraping of GPU titles

--- gpu-price-tracker/pages/api/scraper.ts
+++ gpu-price-tracker/pages/api/scraper.ts
@@ -1,18 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-const cheerio  = require('cheerio');
+const cheerio = require("cheerio");
 
 type Data = {
+  name: string;
+  results?: Array<string>;
+};
+
+type JSONResponse = {
   name: string;
 };
 const Scraper = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const response = await fetch(
-      `https://www.eponuda.com/graficke-kartice-cene`
+      `https://www.monitor.rs/graficke-karte?limit=48&sort=artid_desc&strana=0&price_retail=Min:4050-Max:465355&`
     );
     const htmlString = await response.text();
-    const $ = cheerio.load(htmlString)
-    const searchContext = `div[class='product b-paging-product b-paging-product--vertical']`
+    const $ = cheerio.load(htmlString);
+    const productContext = `.product_single__compact_information`;
+    const nameContext = productContext + ":first"
+    const childContext = productContext + ":first"
+
     // const followerCountString = $(searchContext)
     //   .text()
     // //   .trim()
@@ -20,13 +28,27 @@ const Scraper = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     // //   .join('')
     // console.log(followerCountString)
 
-    const elements = $(searchContext)
-    elements.each((idx:any,el:any) => $(el).text().trim())
-        res.status(200).json({ name: "elements" });
+    const elements = $(productContext);
+    let someObjArr = new Array();
+      elements.contents().each((i:any, el:any ) => {
+        console.log("i: ", i)
+        if ($(el).hasClass('product_single__compact_url') ) {
+          someObjArr.push({name: $(el).text().trim()});
+      }
+      })
+    // const name = $(searchContext)
+    // console.log("array?:", elements.contents());
+    console.log("????:", someObjArr);
 
-  } catch (err) {
+    
+    elements.each((i: any, el: any) => $(el).text().trim());
+    const parsedElements: Array<string> =  elements.text().trim().split("\n");
+    // console.log(parsedElements)
+
+    res.status(200).json({ name: "success" });
+  } catch (err: any) {
     console.error(err);
-    res.status(400).json({ name: "Scraper error" })
+    res.status(400).json({ name: "ERROR" });
   }
 };
 
