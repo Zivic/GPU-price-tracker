03ddb8825f0c965ced2ed9e47b4af58244b457fd
Zivic <djzivic@gmail.com>
2023-02-21 23:52:05 +0100

    style: add additional field for product info sidebar attribute fix

--- gpu-price-tracker/components/Product.tsx
+++ gpu-price-tracker/components/Product.tsx
@@ -1,23 +1,20 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import LineChart from "./LineChart";
-
-
-
 
 type ProductProps = {
   product: {
-    name:string,
-    price:string,
-    id:number,
-  }
-}
+    id: string;
+    name: string;
+    price: number;
+  };
+};
 
 const Product: FC<ProductProps> = ({ product }) => {
   console.log("posts", product);
   console.log("name", product.name);
   console.log("price", product.price);
   console.log("ide", product.id);
-  const  demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+  const demoUrl = "https://codesandbox.io/s/simple-line-chart-kec3v";
 
   return (
     <div>
@@ -25,7 +22,6 @@ const Product: FC<ProductProps> = ({ product }) => {
       <h1>{product.price}</h1>
       <h1>{product.id}</h1>
       <div>
-        
         <div className="flex overflow-hidden bg-white pt-16">
           <div
             id="main-content"
@@ -34,6 +30,41 @@ const Product: FC<ProductProps> = ({ product }) => {
             <main>
               <div className="pt-6 px-4">
                 <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
+
+                {/* PRODUCT INFORMATION */}
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8  2xl:col-span-2">
+                    <div className="flex items-center justify-between mb-4">
+                      <div className="flex-shrink-0">
+                        <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">
+                          {product.name}
+                        </span>
+                        <h3 className="text-base font-normal text-gray-500">
+                          {product.price}
+                        </h3>
+                      </div>
+                      <div className="flex items-center justify-end flex-1 text-green-500 text-base font-bold">
+                        12.5%
+                        <svg
+                          className="w-5 h-5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div id="main-chart2" className="h-96">
+
+                    </div>
+                  </div>
+
+
+
                   <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8  2xl:col-span-2">
                     <div className="flex items-center justify-between mb-4">
                       <div className="flex-shrink-0">
@@ -61,7 +92,7 @@ const Product: FC<ProductProps> = ({ product }) => {
                       </div>
                     </div>
                     <div id="main-chart">
-                  <LineChart/>
+                      <LineChart />
                     </div>
                   </div>
                   <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
