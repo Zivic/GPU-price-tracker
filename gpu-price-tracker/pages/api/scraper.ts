5d6e95dba037e84280fd533deb336ea51454dc6a
Zivic <djzivic@gmail.com>
2023-03-13 21:05:26 +0100

    refactor: generalise webscraper, move parsing Add ScrapedProduct interface and docs Move string parsing logic to specific scraper file Generalise webscraper , now takes in ScrapedProducts

--- gpu-price-tracker/pages/api/scraper.ts
+++ gpu-price-tracker/pages/api/scraper.ts
@@ -14,31 +14,11 @@ type Data = {
 type JSONResponse = {
   name: string;
 };
-
-// var downloadImageToUrl = (
-//   url: string,
-//   filename: string,
-//   callback: Function
-// ) => {
-//   var client = http;
-//   if (url.toString().indexOf("https") === 0) {
-//     client = https;
-//   }
-//   client
-//     .request(url, function (response: any) {
-//       var data = new Stream();
-//       response.on("data", function (chunk: any) {
-//         data.push(chunk);
-//       });
-//       response.on("end", function () {
-//         fs.writeFileSync(filename, data.read());
-//       });
-//     })
-//     .end();
-// };
-
 const Scraper = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
+    //@ts-ignore
+    let retval: ScrapedProduct = {store: "Monitor System"};
+
     const response = await fetch(
       `https://www.monitor.rs/graficke-karte?limit=48&sort=artid_desc&strana=0&price_retail=Min:4050-Max:465355&`
     );
@@ -54,38 +34,41 @@ const Scraper = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     let products = new Array<Object>();
     $(productContext).each((i: any, el: any) => {
-      const name = $(el).children(nameContext).text().trim();
-      const price = $(el)
+      retval.name = $(el).children(nameContext).text().trim();
+
+      const priceAndCurrency: Array<string> = $(el)
         .children(priceContext)
         .children(priceContext2)
         .text()
-        .trim();
+        .trim()
+        .split(" ");
+      //TODO: find a cleaner solution for parsing price
+      retval.price = Number(
+        priceAndCurrency[0].replace(".", "").replace(",00", "")
+      );
+      retval.currency = priceAndCurrency[1];
+
       const information = $(el).children(infoContext).text().trim().split("  ");
+      retval.manufacturer = (information[0] as String)?.split(": ")[1];
+      retval.memoryInterface = (information[1] as String)?.split(": ")[1];
+      retval.memory = (information[2] as String)?.split(": ")[1];
+      retval.processorFrequency = (information[3] as String)?.split(": ")[1];
+      retval.memoryFrequency = (information[4] as String)?.split(": ")[1];
+      retval.guarantee = (information[5] as String)?.split(": ")[1];
+
       const image: string = $(el)
         .children(imageWrapperContext)
         .children(imageContext)
         .attr("src");
-      const bigImage  = image.replace("200x200", "1000x1000")
+      const bigImage = image.replace("200x200", "1000x1000");
       const fullImageLink = "https://www.monitor.rs/" + bigImage;
       const img_name_clean = image.replace(/^(.+?\.(gif|png|jpe?g)).*$/i, "$1");
-      console.log(fullImageLink);
+      // console.log(fullImageLink);
+      // console.log(img_name_clean);
+      retval.fullImageLink = fullImageLink
+      // console.log("retval", retval);
 
-      console.log(img_name_clean);
-      // const filePath =
-      //   "/home/djole/Documents/Repo/GPU-price-tracker/gpu-price-tracker/images/" +
-      //   img_name_clean;
-      // downloadImageToUrl(fullImageLink, filePath, () => {});
-      // try{
-      //   fs.createWriteStream('/home/djole/Documents/Repo/GPU-price-tracker/gpu-price-tracker/images/'+img_name_clean)
-      // }
-      // catch {
-      //   console.log("Error saving image to file system");
-      // }
-      // console.log("NAME:", name);
-      // console.log("PRICE", price);
-      // console.log("image:", image);
-
-      products.push({ name: name, price: price, full_image_link: fullImageLink, information: information });
+      products.push(retval);
     });
 
     res.status(200).json({ name: "success", results: products });
