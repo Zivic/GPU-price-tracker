99b95ca32faf04fee8106334722b5ffd5215a035
Zivic <djzivic@gmail.com>
2023-03-20 20:22:11 +0100

    perf: check if image exists before uploading

--- gpu-price-tracker/utils/data/cloudinary.ts
+++ gpu-price-tracker/utils/data/cloudinary.ts
@@ -1,34 +1,39 @@
-import * as dotenv from 'dotenv';
-const cloudinary = require('cloudinary').v2;
+import * as dotenv from "dotenv";
+const cloudinary = require("cloudinary").v2;
 
 const UploadImage = (image: string, publicId: string) => {
-dotenv.config();
+  dotenv.config();
 
-// Configuration 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-});
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
 
-// Upload
-const res = cloudinary.uploader.upload(image, {public_id: publicId})
+  // Upload
+  const res = cloudinary.uploader.upload(image, {
+    public_id: publicId,
+    overwrite: false,
+  });
 
-res.then((data: any) => {
-  console.log(data);
-  // console.log(data.secure_url);
-}).catch((err: Error) => {
-  console.log(err);
-});
+  res
+    .then((data: any) => {
+      console.log(data);
+      // console.log(data.secure_url);
+    })
+    .catch((err: Error) => {
+      console.log(err);
+    });
 
-// Generate 
-const url = cloudinary.url(publicId, {
-//   width: 100,
-//   height: 150,
-  // Crop: 'fill'
-});
+  // Generate
+  const url = cloudinary.url(publicId, {
+    //   width: 100,
+    //   height: 150,
+    // Crop: 'fill'
+  });
 
-return url;
-}
+  return url;
+};
 
-export {UploadImage};
+export { UploadImage };
