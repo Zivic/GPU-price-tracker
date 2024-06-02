6f437fc09d3b484404f1e9d83b673d15341a2a9c
Zivic <djzivic@gmail.com>
2024-06-02 21:59:53 +0200

    feat: working locomotive scroll downgrade to version 4 remove eslint enable overflow x

--- gpu-price-tracker/app/landing/page.tsx
+++ gpu-price-tracker/app/landing/page.tsx
@@ -1,23 +1,38 @@
-"use client"
+"use client";
 import "./landing.scss";
 // import LocomotiveScroll from "locomotive-scroll";
-import { useEffect } from "react";
+import { useLayoutEffect, useEffect, useRef } from "react";
 export default function Landing() {
+  const horizontalContainer = useRef(null);
+  const verticalContainer = useRef(null);
+  const section = useRef(null);
+  const yo = useRef(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    let scroll = null;
     async function getLocomotive() {
-      const Locomotive = (await import("locomotive-scroll")).default;
-      // const locomotiveScroll = new Locomotive.LocomotiveScroll();
-      const scroll = new Locomotive()
-    }
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
 
+        scroll = new LocomotiveScroll({
+        el: section.current,
+        // el: verticalContainer.current,
+        smooth: true,
+        direction: "vertical",
+      });
+    }
     getLocomotive();
+
+    return(()=> {
+      scroll.destroy();
+    })
   }, []);
+
   // const locomotiveScroll = new LocomotiveScroll();
   // if (!window) throw new Error("Window not defined");
   return (
-    <section className="">
-      <div className="absolute flex w-full h-full z-10 justify-center items-center">
+    <section ref={section} className="data-scroll-container">
+      <div  data-scroll data-scroll-speed="0.1" data-scroll-direction="horizontal" >
+      <div  className="absolute flex w-full h-full z-10 justify-center items-center">
         <div className=" mb-96">
           <h1 className="font-kinetika">GPU PRICE TRACKER</h1>
         </div>
@@ -40,24 +55,87 @@ export default function Landing() {
           />
         </video>
       </div>
-      <div className=" h-screen flex">
-        <div className=" text-8xl font-kinetika font-extrabold m-32 w-full">
-          <h1> One stop design shop for your digital product 👋</h1>
-        </div>
       </div>
-      <div>
-        <div className="text-sm font-kinetika leading-2 w-full">
-          <h2 data-scroll data-scroll-speed="0.1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h2>
-          <p data-scroll data-scroll-speed="0.8">
-            😬
-          </p>
+
+
+        <div data-scroll-section className=" h-screen flex">
+          <div className=" text-8xl font-kinetika font-extrabold m-32 w-full">
+            <h1> One stop design shop for your digital product 👋</h1>
+          </div>
         </div>
         <div>
-          <div className=" w-48 h-48 bg-slate-500 rounded-lg"></div>
+          <div className=" h-screen  text-sm font-kinetika m-32 leading-normal w-full">
+            <h2 data-scroll data-scroll-speed="0.4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h2>
+            <p data-scroll data-scroll-speed="0.1">
+              😬
+            </p>
+          </div>
         </div>
-      </div>
+
+        <div
+          ref={horizontalContainer}
+          data-scroll-section
+          className="horizontal-container h-screen flex "
+        >
+          <div ref={yo}  className="flex m-32 w-full gap-20">
+            <div
+              data-scroll
+              data-scroll-speed="0.3"
+              data-scroll-direction="horizontal"
+              className=" w-48 h-48 bg-slate-600 rounded-lg"
+            ></div>
+            <div
+              data-scroll
+              data-scroll-speed="0.4"
+              data-scroll-direction="horizontal"
+              className=" w-48 h-48 bg-slate-700 rounded-lg"
+            ></div>
+            <div
+              data-scroll
+              data-scroll-speed="0.8"
+              data-scroll-direction="horizontal"
+              className=" w-48 h-48 bg-slate-800 rounded-lg"
+            ></div>
+            <div>
+              <div
+                data-scroll
+                data-scroll-speed="0.1"
+                data-scroll-direction="horizontal"
+                className=" w-48 h-48 bg-slate-900 rounded-lg"
+              ></div>
+              <div
+                data-scroll
+                data-scroll-speed="0.1"
+                data-scroll-direction="horizontal"
+
+                className=" w-48 h-48 bg-slate-600 rounded-lg"
+              ></div>
+              <div
+                data-scroll
+                data-scroll-speed="0.1"
+                data-scroll-direction="horizontal"
+
+                className=" w-48 h-48 bg-slate-700 rounded-lg"
+              ></div>
+              <div
+                data-scroll
+                data-scroll-speed="0.1"
+                data-scroll-direction="horizontal"
+
+                className=" w-48 h-48 bg-slate-800 rounded-lg"
+              ></div>
+              <div
+                data-scroll
+                data-scroll-speed="0.1"
+                data-scroll-direction="horizontal"
+
+                className=" w-48 h-48 bg-slate-900 rounded-lg"
+              ></div>
+            </div>
+          </div>
+        </div>
     </section>
   );
 }
