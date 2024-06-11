96860f97dd4a389f1cb8243347cdef1be2e9de2d
Zivic <djzivic@gmail.com>
2024-06-11 22:38:53 +0200

    feat: restore style, stable horizontal scroll

--- gpu-price-tracker/app/landing/page.tsx
+++ gpu-price-tracker/app/landing/page.tsx
@@ -101,120 +101,113 @@ export default function Landing() {
     initGSAP();
   }, [initialized, scroller]);
 
-
   return (
     <>
-      
-
       <div id="scroller">
-        <section className="blank">
-          <h1>ScrollTrigger and Locomotive-Scroll</h1>
-          <p>...</p>
-        </section>
-
-        
-        {/* <section >
-        <div>
-          <div className="absolute flex w-full h-full z-10 justify-center items-center">
-            <div className=" mb-96">
-              <h1 className="font-kinetika">GPU PRICE TRACKER</h1>
+        <section className="main">
+          <div>
+            <div className="absolute flex w-full h-full z-10 justify-center items-center">
+              <div className=" mb-96">
+                <h1 className="font-kinetika">GPU PRICE TRACKER</h1>
+              </div>
+            </div>
+            <div className="noiseOverlay">
+              <video
+                className="mouse-md opacity-40"
+                loop={true}
+                muted
+                autoPlay
+                playsInline
+                // poster={}
+                data-aos="zoom-in-down"
+              >
+                <source
+                  src={
+                    "https://res.cloudinary.com/dyudvhug1/video/upload/v1716148067/vlc-record-2024-05-18-15h48m58s-ROG_Strix_GeForce_RTX_2080_Ti_and_2080_Graphics_Cards_Victory_Redefined-_erj2nz.mp4"
+                  }
+                  type="video/mp4"
+                />
+              </video>
             </div>
           </div>
-          <div className="noiseOverlay">
-            <video
-              className="mouse-md opacity-40"
-              loop={true}
-              muted
-              autoPlay
-              playsInline
-              // poster={}
-              data-aos="zoom-in-down"
-            >
-              <source
-                src={
-                  "https://res.cloudinary.com/dyudvhug1/video/upload/v1716148067/vlc-record-2024-05-18-15h48m58s-ROG_Strix_GeForce_RTX_2080_Ti_and_2080_Graphics_Cards_Victory_Redefined-_erj2nz.mp4"
-                }
-                type="video/mp4"
-              />
-            </video>
-          </div>
-        </div>
 
-        <div data-scroll-section className=" h-screen flex">
-          <div className=" text-8xl font-kinetika font-extrabold m-32 w-full">
-            <h1> One stop design shop for your digital product 👋</h1>
+          <div data-scroll-section className=" h-screen flex">
+            <div className=" text-8xl font-kinetika font-extrabold m-32 w-full">
+              <h1> One stop design shop for your digital product 👋</h1>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className=" h-screen  text-sm font-kinetika m-32 leading-normal w-full">
-            <h2 data-scroll data-scroll-speed="0.4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h2>
-            <p data-scroll data-scroll-speed="0.1">
-              😬
-            </p>
+          <div>
+            <div className=" h-screen  text-sm font-kinetika m-32 leading-normal w-full">
+              <h2 data-scroll data-scroll-speed="0.4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
+              <p data-scroll data-scroll-speed="0.1">
+                😬
+              </p>
+            </div>
           </div>
-        </div>
         </section>
-        <section
-          className="horizontal h-screen flex "
-        >
+
+        <section className=" h-screen flex ">
           <div className="pin-wrap">
-          <div className="animation-wrap to-right flex m-32 w-full gap-20">
-            <div
-              data-scroll
-              data-scroll-speed="0.3"
-              data-scroll-direction="horizontal"
-              className=" w-48 h-48 bg-slate-600 rounded-lg"
-            ></div>
-            <div
-              data-scroll
-              data-scroll-speed="0.4"
-              data-scroll-direction="horizontal"
-              className=" w-48 h-48 bg-slate-700 rounded-lg"
-            ></div>
-            <div
-              data-scroll
-              data-scroll-speed="0.8"
-              data-scroll-direction="horizontal"
-              className=" w-48 h-48 bg-slate-800 rounded-lg"
-            ></div>
-            <div>
+            <div className="animation-wrap to-right flex m-32 w-full gap-20">
               <div
                 data-scroll
-                data-scroll-speed="0.1"
-                data-scroll-direction="horizontal"
-                className=" w-48 h-48 bg-slate-900 rounded-lg"
-              ></div>
-              <div
-                data-scroll
-                data-scroll-speed="0.1"
+                data-scroll-speed="0.3"
                 data-scroll-direction="horizontal"
                 className=" w-48 h-48 bg-slate-600 rounded-lg"
               ></div>
               <div
                 data-scroll
-                data-scroll-speed="0.1"
+                data-scroll-speed="0.4"
                 data-scroll-direction="horizontal"
                 className=" w-48 h-48 bg-slate-700 rounded-lg"
               ></div>
               <div
                 data-scroll
-                data-scroll-speed="0.1"
+                data-scroll-speed="0.8"
                 data-scroll-direction="horizontal"
                 className=" w-48 h-48 bg-slate-800 rounded-lg"
               ></div>
-              <div
-                data-scroll
-                data-scroll-speed="0.1"
-                data-scroll-direction="horizontal"
-                className=" w-48 h-48 bg-slate-900 rounded-lg"
-              ></div>
+              <div>
+                <div
+                  data-scroll
+                  data-scroll-speed="0.1"
+                  data-scroll-direction="horizontal"
+                  className=" w-48 h-48 bg-slate-900 rounded-lg"
+                ></div>
+                <div
+                  data-scroll
+                  data-scroll-speed="0.1"
+                  data-scroll-direction="horizontal"
+                  className=" w-48 h-48 bg-slate-600 rounded-lg"
+                ></div>
+                <div
+                  data-scroll
+                  data-scroll-speed="0.1"
+                  data-scroll-direction="horizontal"
+                  className=" w-48 h-48 bg-slate-700 rounded-lg"
+                ></div>
+                <div
+                  data-scroll
+                  data-scroll-speed="0.1"
+                  data-scroll-direction="horizontal"
+                  className=" w-48 h-48 bg-slate-800 rounded-lg"
+                ></div>
+                <div
+                  data-scroll
+                  data-scroll-speed="0.1"
+                  data-scroll-direction="horizontal"
+                  className=" w-48 h-48 bg-slate-900 rounded-lg"
+                ></div>
+              </div>
             </div>
           </div>
-          </div>
-      </section> */}
-
+        </section>
+        <section className="blank">
+          <h1>ScrollTrigger and Locomotive-Scroll</h1>
+          <p>...</p>
+        </section>
 
         <section className="horizontal">
           <div className="pin-wrap">
