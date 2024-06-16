394d6f82588b51481cdabefbb19f4bd8acefcbaa
Zivic <djzivic@gmail.com>
2024-06-16 23:22:21 +0200

    style: add more manufacturer images manually scaled with viewBox

--- gpu-price-tracker/components/ScrollManufacturers.tsx
+++ gpu-price-tracker/components/ScrollManufacturers.tsx
@@ -4,37 +4,41 @@ const ScrollManufacturers = () => {
   // const companyNames = ["Tehnomedia","Monitor System","Jakov","Gigatron", "ITShop"]
   const manufacturers = [
     {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
-    {
-      name: "Nvidia",
-      imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
-    },
+        name: "Nvidia",
+        imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
+      },
+      {
+        name: "MSI",
+        imageLink: "url(/msi.svg)",
+      },
+      {
+        name: "ASUS",
+        imageLink: "url(/asus.svg)",
+      },
+      {
+        name: "Nvidia",
+        imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
+      },
+      {
+        name: "MSI",
+        imageLink: "url(/msi.svg)",
+      },
+      {
+        name: "ASUS",
+        imageLink: "url(/asus.svg)",
+      },
+      {
+        name: "Nvidia",
+        imageLink: "url(/Nvidia-Horizontal-Black-Logo.wine.svg)",
+      },
+      {
+        name: "MSI",
+        imageLink: "url(/msi.svg)",
+      },
+      {
+        name: "ASUS",
+        imageLink: "url(/asus.svg)",
+      },
   ];
 
   //   style={{backgroundImage: manufacturer.imageLink}}
